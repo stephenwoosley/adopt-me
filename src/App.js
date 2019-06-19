ac5f0cbbd6b6057f5b0812f45e28e6d0efb14e3d
Stephen Woosley <stephen.woosley@fmr.com>
2019-06-19 16:45:20 -0400

    create initial HTML document, React with no JSX

---
+++ src/App.js
@@ -0,0 +1,23 @@
+
+const Pet = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Niko"),
+    React.createElement("h2", {}, "Dog"),
+    React.createElement("h2", {}, "Staffordshire Terrier")
+  ]);
+};
+
+
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet),
+    React.createElement(Pet),
+    React.createElement(Pet)
+  ]);
+};
+
+ReactDOM.render(
+  React.createElement(App),
+  document.getElementById("root")
+);
